1f026d0fe5fa38d683791b7a9d2982040c9cd8c9
emonShimoul <emonshimoul@gmail.com>
2022-05-17 23:41:02 +0600

    uses of querySelector with addEventListener

--- js-basics/DOM/js/dom-main.js
+++ js-basics/DOM/js/dom-main.js
@@ -33,12 +33,23 @@ document.getElementById("header").setAttribute("style", "border: 1px solid green
 
 // document.getElementById("header").removeAttribute("class");
 
-element = document.querySelector(".list");
-element = document.querySelectorAll(".list");
-element = document.querySelectorAll(".list")[1];
-element = document.querySelectorAll("ul")[0].innerHTML;
-element = document.querySelectorAll("#header h1");
+// element = document.querySelector(".list");
+// element = document.querySelectorAll(".list");
+// element = document.querySelectorAll(".list")[1];
+// element = document.querySelectorAll("ul")[0].innerHTML;
+// element = document.querySelectorAll("#header h1");
+element = document.querySelectorAll("#header h1")[1].innerHTML;
 
+document.querySelector("#header").style.backgroundColor = "tan";
+document.querySelector("#header").style.color = "#fff";
+
+document.querySelector("#header").addEventListener("mouseenter", abc);
+document.querySelector("#header").addEventListener("click", function(){
+    document.querySelector("#header").style.border = "10px solid green";
+});
+function abc(){
+    document.querySelector("#header").style.background = "crimson";
+}
 
 
 console.log(element);
