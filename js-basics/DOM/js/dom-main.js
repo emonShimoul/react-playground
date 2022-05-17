0eb16b235544034f4b84f8ff50e742897fb37798
emonShimoul <emonshimoul@gmail.com>
2022-05-17 23:39:50 +0600

    uses of setAttribute and removeAttribute

--- js-basics/DOM/js/dom-main.js
+++ js-basics/DOM/js/dom-main.js
@@ -13,15 +13,25 @@ var element;
 // element = document.getElementsByTagName("ul");
 // element = document.getElementsByTagName("ul")[2];
 
-element = document.getElementById("header").innerText;
-element = document.getElementById("content").innerText;
-element = document.getElementById("content").innerHTML;
-element = document.getElementById("header").getAttribute("class");
-element = document.getElementById("header").getAttributeNode("style");
-element = document.getElementById("header").attributes;
-element = document.getElementById("header").attributes[2];
-element = document.getElementById("header").attributes[2].value;
-element = document.getElementById("header").attributes[2].name;
+// element = document.getElementById("header").innerText;
+// element = document.getElementById("content").innerText;
+// element = document.getElementById("content").innerHTML;
+// element = document.getElementById("header").getAttribute("class");
+// element = document.getElementById("header").getAttributeNode("style");
+// element = document.getElementById("header").attributes;
+// element = document.getElementById("header").attributes[2];
+// element = document.getElementById("header").attributes[2].value;
+// element = document.getElementById("header").attributes[2].name;
+
+// document.getElementById("header").innerHTML = "<h1>JavaScript</h1>";
+// document.querySelector("#header").innerHTML = "<h1>JS</h1>";
+
+// document.getElementById("header").setAttribute("class", "xyz");
+// document.getElementById("header").attributes[1].value = "xyz";
+
+// document.getElementById("header").setAttribute("style", "border: 1px solid green");
+
+document.getElementById("header").removeAttribute("class");
 
 
 
