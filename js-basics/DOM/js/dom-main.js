8d7779bf8adb314c4c9c95a65c0eeced352f0278
emonShimoul <emonshimoul@gmail.com>
2022-05-17 23:37:17 +0600

    uses js dom

---
+++ js-basics/DOM/js/dom-main.js
@@ -0,0 +1,16 @@
+var element;
+
+// element = document.body;
+// element = document.baseURI;
+// element = document.URL;
+// element = document.domain;
+// element = document.images;
+
+
+element = document.getElementById("header");
+element = document.getElementsByClassName("list");
+element = document.getElementsByClassName("list")[0];
+element = document.getElementsByTagName("ul");
+element = document.getElementsByTagName("ul")[2];
+
+console.log(element);
