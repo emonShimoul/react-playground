cfc0181998d4ff877fc15114747ddcc9ea4c36eb
emonShimoul <emonshimoul@gmail.com>
2022-05-17 23:38:30 +0600

    uses of document.getElementById

--- js-basics/DOM/js/dom-main.js
+++ js-basics/DOM/js/dom-main.js
@@ -7,10 +7,23 @@ var element;
 // element = document.images;
 
 
-element = document.getElementById("header");
-element = document.getElementsByClassName("list");
-element = document.getElementsByClassName("list")[0];
-element = document.getElementsByTagName("ul");
-element = document.getElementsByTagName("ul")[2];
+// element = document.getElementById("header");
+// element = document.getElementsByClassName("list");
+// element = document.getElementsByClassName("list")[0];
+// element = document.getElementsByTagName("ul");
+// element = document.getElementsByTagName("ul")[2];
+
+element = document.getElementById("header").innerText;
+element = document.getElementById("content").innerText;
+element = document.getElementById("content").innerHTML;
+element = document.getElementById("header").getAttribute("class");
+element = document.getElementById("header").getAttributeNode("style");
+element = document.getElementById("header").attributes;
+element = document.getElementById("header").attributes[2];
+element = document.getElementById("header").attributes[2].value;
+element = document.getElementById("header").attributes[2].name;
+
+
+
 
 console.log(element);
