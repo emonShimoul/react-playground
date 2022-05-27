2d0f1088931850d638026eb7728e440f3a08dedc
emonShimoul <emonshimoul@gmail.com>
2022-05-27 23:35:16 +0600

    remove unnecessary code from script1.js

--- js-basics/script1.js
+++ js-basics/script1.js
@@ -1,11 +1,3 @@
-// function abc(a,b){
-//     let c = a + b;
-//     return c;
-// }
-
-// var sum = abc(15,20);
-// console.log(sum);
-
 var ages = [10,12,11,16];
 console.log(ages);
 var b = ages.filter(checkOut);
