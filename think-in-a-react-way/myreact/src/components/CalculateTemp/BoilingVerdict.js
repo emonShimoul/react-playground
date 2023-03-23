518038168e6a0637c8afb694e76aecb7d768306d
emonShimoul <emonshimoul@gmail.com>
2023-03-23 16:40:21 +0600

    react lifting state up

--- think-in-a-react-way/myreact/src/components/CalculateTemp/BoilingVerdict.js
+++ think-in-a-react-way/myreact/src/components/CalculateTemp/BoilingVerdict.js
@@ -3,7 +3,7 @@ import React from 'react';
 const BoilingVerdict = ({celsius}) => {
     return (
         <div>
-            {celsius>=100 ? "Water would boil." : "Water would not boil."}
+            {celsius>=100 ? "Water would boil !!" : "Water would not boil !!"}
         </div>
     );
 };
