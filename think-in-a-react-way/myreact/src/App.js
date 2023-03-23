ca98373d5cf76750e1ef6501fb7cb0c25fdd3bde
emonShimoul <emonshimoul@gmail.com>
2023-03-24 01:20:38 +0600

    react form handling

--- think-in-a-react-way/myreact/src/App.js
+++ think-in-a-react-way/myreact/src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
-import Calculator from './components/CalculateTemp/Calculator';
-import ClockList from './components/ClockList';
 import Form from './components/Form';
 
 function App() {
-  const quantities = [1,2,3];
   return (
     <div className="App">
       <header className="App-header">
-        <ClockList quantities={quantities} />
         <Form />
-        <Calculator />
       </header>
     </div>
   );
