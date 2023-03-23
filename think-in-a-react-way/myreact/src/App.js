fd2db99000b6b728eb7de00a4ebe81378c4f71e5
emonShimoul <emonshimoul@gmail.com>
2023-03-24 00:50:39 +0600

    react render props

--- think-in-a-react-way/myreact/src/App.js
+++ think-in-a-react-way/myreact/src/App.js
@@ -1,41 +1,24 @@
 import './App.css';
 import ClickCounter from './components/ClickCounter';
+import Counter from './components/Counter';
 import HoverCounter from './components/HoverCounter';
-// import Calculator from './components/CalculateTemp/Calculator';
-// import ClockList from './components/ClockList';
-// import Form from './components/Form';
-// import Emoji from './components/composition/Emoji';
-// import Text from './components/composition/Text';
-// import Bracket from './components/composition/Bracket';
 
 function App() {
   // const quantities = [1,2,3];
   return (
     <div className="App">
       <header className="App-header">
-        {/* <ClockList quantities={quantities} /> */}
-        {/* <Form /> */}
-        {/* <Calculator /> */}
+        <Counter 
+          render={(counter, incrementCount) => (
+            <ClickCounter count={counter} incrementCount={incrementCount} />
+          )} 
+        />
 
-        {/* <Text /> */}
-
-        {/* use of composition in react */}
-        {/* <Emoji>
-          {({addEmoji})=> <Text addEmoji={addEmoji}/>}
-        </Emoji> */}
-
-        {/* use of composition in multiple and nested component  */}
-        {/* <Emoji>
-          {({addEmoji})=> (
-            <Bracket>
-              {({addBracket})=> <Text addEmoji={addEmoji} addBracket={addBracket}/>}
-            </Bracket>
-          )}
-        </Emoji> */}
-
-        {/* used for the example of HOC component */}
-        <ClickCounter />
-        <HoverCounter />
+        <Counter 
+          render={(counter, incrementCount) => (
+            <HoverCounter count={counter} incrementCount={incrementCount} />
+          )} 
+        />
       </header>
     </div>
   );
