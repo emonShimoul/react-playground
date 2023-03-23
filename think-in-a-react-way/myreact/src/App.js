0fdb6e1e2b1e0625dfc0d8ea0358ea232e88c05a
emonShimoul <emonshimoul@gmail.com>
2023-03-23 17:45:34 +0600

    react composition vs inheritance

--- think-in-a-react-way/myreact/src/App.js
+++ think-in-a-react-way/myreact/src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import Calculator from './components/CalculateTemp/Calculator';
 import ClockList from './components/ClockList';
 import Form from './components/Form';
-import Text from './components/inheritance/Text';
+import Emoji from './components/composition/Emoji';
+import Text from './components/composition/Text';
+import Bracket from './components/composition/Bracket';
 
 function App() {
   // const quantities = [1,2,3];
@@ -12,7 +14,23 @@ function App() {
         {/* <ClockList quantities={quantities} /> */}
         {/* <Form /> */}
         {/* <Calculator /> */}
+
         <Text />
+
+        {/* use of composition in react */}
+        <Emoji>
+          {({addEmoji})=> <Text addEmoji={addEmoji}/>}
+        </Emoji>
+
+        {/* use of composition in multiple and nested component  */}
+        <Emoji>
+          {({addEmoji})=> (
+            <Bracket>
+              {({addBracket})=> <Text addEmoji={addEmoji} addBracket={addBracket}/>}
+            </Bracket>
+          )}
+        </Emoji>
+
       </header>
     </div>
   );
