faa6ad6d45f6f31208eb9ceb3b7b183eb94d7368
emonShimoul <emonshimoul@gmail.com>
2023-03-24 01:24:46 +0600

    react lifting state up

--- think-in-a-react-way/myreact/src/App.js
+++ think-in-a-react-way/myreact/src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
 import Calculator from './components/CalculateTemp/Calculator';
-import ClockList from './components/ClockList';
-import Form from './components/Form';
 
 function App() {
-  const quantities = [1,2,3];
   return (
     <div className="App">
       <header className="App-header">
-        <ClockList quantities={quantities} />
-        <Form />
         <Calculator />
       </header>
     </div>
