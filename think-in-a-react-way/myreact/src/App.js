529d8199dd9e509b56fa20fa21c091b92f0784c3
emonShimoul <emonshimoul@gmail.com>
2023-03-24 01:30:31 +0600

    react conditional rendering

--- think-in-a-react-way/myreact/src/App.js
+++ think-in-a-react-way/myreact/src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
-import Calculator from './components/CalculateTemp/Calculator';
 import ClockList from './components/ClockList';
-import Form from './components/Form';
 
 function App() {
   const quantities = [1,2,3];
@@ -9,8 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <ClockList quantities={quantities} />
-        <Form />
-        <Calculator />
       </header>
     </div>
   );
