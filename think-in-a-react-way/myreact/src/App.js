467b844ff48e6c167b753ceb71969ec290bd6596
emonShimoul <emonshimoul@gmail.com>
2023-03-28 07:39:05 +0600

    react styled components

--- think-in-a-react-way/myreact/src/App.js
+++ think-in-a-react-way/myreact/src/App.js
@@ -1,11 +1,15 @@
-import './assets/css/global.css';
-import Logo from './components/Logo';
+// import './assets/css/dark.css';
+// import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './components/styles/Global.styles';
+import Tutorial from './components/Tutorial';
 
 export default function App() {
     return (
-        <div className="App">
-            <h1>Learn with Sumit</h1>
-            <Logo />
-        </div>
+        <>
+            <GlobalStyle />
+            {/* <ThemeProvider theme={"dark"}> */}
+                <Tutorial />
+            {/* </ThemeProvider> */}
+        </>
     );
 }
