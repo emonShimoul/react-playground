ae2e29a6c8700b77fd2ac801fbee50ff5a751e3f
emonShimoul <emonshimoul@gmail.com>
2023-04-06 06:54:28 +0600

    result page completed

---
+++ think-in-a-react-way/quiz-project/src/hooks/useAnswers.js
@@ -0,0 +1,43 @@
+import { get, getDatabase, orderByKey, query, ref } from "firebase/database";
+import { useEffect, useState } from "react";
+
+const useAnswers = (videoID) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [answers, setAnswers] = useState([]);
+
+  useEffect(() => {
+    async function fetchAnswers() {
+      // database related works
+      const db = getDatabase();
+      const answersRef = ref(db, "answers/" + videoID + "/questions");
+      const answerQuery = query(answersRef, orderByKey());
+
+      try {
+        setError(false);
+        setLoading(true);
+        // request firebase database
+        const snapshot = await get(answerQuery);
+        setLoading(false);
+        if (snapshot.exists()) {
+          setAnswers((prevAnswers) => {
+            return [...prevAnswers, ...Object.values(snapshot.val())];
+          });
+        }
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+        setError(true);
+      }
+    }
+    fetchAnswers();
+  }, [videoID]);
+
+  return {
+    loading,
+    error,
+    answers,
+  };
+};
+
+export default useAnswers;
